8f6fa3e0a0e650fc47d8246e4b04d05250a7540d
pluralparallel <gregory.operto@gmail.com>
2017-01-29 00:31:10 +0100

    [ref] moved first message to specific js file

---
+++ js/firstmessage.js
@@ -0,0 +1,21 @@
+
+var Title = Title || {};
+
+Title.createTitle = function(){
+
+   Title.title = game.add.bitmapText(100, 100, 'ubuntu', 'Dans trois minutes au plus tard, \n'+
+                                                   '  je devrai avoir quitte cet endroit \n'+
+                                                      'une fois pour toutes...', 14);
+   Title.title.alpha = 0.1;
+
+   Title.fadeIn = game.add.tween(Title.title);
+   Title.fadeIn.to( { alpha: 1 }, 4000, "Linear", true);
+   Title.fadeIn.onComplete.add(Title.fadeOut, this);
+}
+
+Title.fadeOut = function(){
+
+   Title.fadeOut = game.add.tween(Title.title);
+   Title.fadeOut.to( { alpha: 0 }, 4000, "Linear", true);
+
+}
